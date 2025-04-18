5e11e8120d956763fa6979f68e086d9b61178c25
Nuraly <abdrashevnuraly72@gmail.com>
2025-04-19 00:38:00 +0500

    routing navigation

--- angular_part/src/app/app.routes.ts
+++ angular_part/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { CarsComponent } from './cars/cars.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+export const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'cars', component: CarsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: '**', redirectTo: '/about' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
